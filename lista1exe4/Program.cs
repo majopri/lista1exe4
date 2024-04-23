fc1c32870f9175721ec661a83586806e6a1b4b2b
User <User@DESKTOP-G0A3IIV>
2024-04-22 22:13:55 -0300

    Adicionar arquivos de projeto.

---
+++ lista1exe4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1exe4
+{
+    internal class Program
+    {
+        private static object consolie;
+
+        public static object Consolie { get; private set; }
+
+        static void Main(string[] args)
+        {
+            double baseTriangulo, alturaTriangulo, areaTriangulo;
+
+            // solicita e lê a base do triangulo
+            Console.WriteLine("Digite o valor da base do triangulo:");
+            baseTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            // solicita e lê a altura do triangulo
+            Console.WriteLine("Digite o valor da altura do triangulo:");
+            alturaTriangulo = Convert.ToDouble(Console.ReadLine());
+
+            // calcula a área do 
+            areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+
+            //Exibe a área do triangulo
+            Console.WriteLine("A área do triangulo é: " + areaTriangulo);
+
+        }
+    }
+}
